69a72b5daba402c3b5b5002a61d2392225c716f0
Shashank-H <shashankharikripa@gmail.com>
2022-08-06 15:34:20 +0530

    Minor improvemnets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
+import 'antd/dist/antd.min.css';
 import './App.css';
-import 'antd/dist/antd.css';
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import { AppHeader } from './components/AppHeader';
-import { AppSideBar } from './components/AppSideBar';
-import { MainContent, MainLayout } from './components/LayoutStyled';
-import { AddOrderPage } from './pages/AddOrder/AddOrderPage';
-import { OrderPage } from './pages/Orders/OrderPage';
-import { ViewOrderPage } from './pages/ViewOrder/ViewOrderPage';
+import Main from './Main';
+import { AppProvider } from './state/Provider';
 
 function App() {
   return (
-    <BrowserRouter>
-      <MainLayout>
-        {/* <SideBar>
-          <NavLink to={"add-order"}>go to add</NavLink>
-        </SideBar> */}
-        <AppSideBar />
-          <MainContent>
-            <AppHeader />
-            <Routes>
-              <Route path="/" element={<OrderPage />} />
-              <Route path="orders" element={<OrderPage />} />
-              <Route path="add-order" element={<AddOrderPage />} />
-              <Route path="view-order/:id" element={<ViewOrderPage/>} />
-              {/* <Route path="details" element={<AddOrderPage />} /> */}
-              <Route path="config" element={<>Config Page</>} />
-              <Route path="team" element={<>Team Page</>} />
-
-            </Routes>
-          </MainContent>
-      </MainLayout>
-    </BrowserRouter>
+    <AppProvider>
+      <Main />
+    </AppProvider>
   );
 }
 
---
+++ src/Main.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { AppHeader } from './components/AppHeader';
+import { AppSideBar } from './components/AppSideBar';
+import { MainContent, MainLayout } from './components/LayoutStyled';
+import { AddOrderPage } from './pages/AddOrder/AddOrderPage';
+import { OrderPage } from './pages/Orders/OrderPage';
+import { ViewOrderPage } from './pages/ViewOrder/ViewOrderPage';
+
+function Main() {
+  return (
+    <BrowserRouter>
+      <MainLayout>
+        {/* <SideBar>
+          <NavLink to={"add-order"}>go to add</NavLink>
+        </SideBar> */}
+        <AppSideBar />
+          <MainContent>
+            <AppHeader />
+            <Routes>
+              <Route path="/" element={<OrderPage />} />
+              <Route path="orders" element={<OrderPage />} />
+              <Route path="add-order" element={<AddOrderPage />} />
+              <Route path="view-order/:id" element={<ViewOrderPage/>} />
+              {/* <Route path="details" element={<AddOrderPage />} /> */}
+              <Route path="config" element={<>Config Page</>} />
+              <Route path="team" element={<>Team Page</>} />
+
+            </Routes>
+          </MainContent>
+      </MainLayout>
+    </BrowserRouter>
+  );
+}
+
+export default Main;
